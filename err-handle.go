828e283e8333b83b416fe864014a2874e8a3fbae
subharad <subharad@cisco.com>
2023-08-22 09:42:47 +0530

    err

---
+++ err-handle.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var DivideByZero = errors.New("DivideByZero error occured")
+
+func main() {
+
+	divide(3, 4)
+}
+
+func checkDivideByZero(y int) error {
+	if y == 0 {
+		return DivideByZero
+	}
+	return nil
+}
+func divide(x, y int) {
+	val := checkDivideByZero(y)
+	if val == nil {
+		quotient := x / y
+		remainder := x % y
+		fmt.Printf("Dividing %d by %d, quotient = %d and remainder = %d\n", x, y, quotient, remainder)
+	}
+}
